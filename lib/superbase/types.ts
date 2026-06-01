e2cc565fe75823f37554a19499d15568a0c644ca
ArtyomArtenyan <artyomartenyan.ait@gmail.com>
2026-06-02 00:58:10 +0400

    chore(supabase): add database types

---
+++ lib/superbase/types.ts
@@ -0,0 +1,28 @@
+export type Board = {
+	id: number;
+	created_at: string;
+	updated_at: string;
+	title: string;
+	description: string | null;
+	color: string | null;
+	user_id: string;
+};
+export type Column = {
+	id: number;
+	created_at: string;
+	board_id: number;
+	title: string;
+	sort_order: number;
+	user_id: string;
+};
+export type Task = {
+	id: number;
+	created_at: string;
+	title: string;
+	description: string | null;
+	assignee: string | null;
+	due_date: string | null;
+	priority: 'low' | 'medium' | 'high';
+	sort_order: number;
+	column_id: number;
+};
