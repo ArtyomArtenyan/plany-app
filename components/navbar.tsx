2046155a7a08468fd0163c1caab10cf5c56ef0fe
ArtyomArtenyan <artyomartenyan.ait@gmail.com>
2026-06-05 16:24:17 +0400

    refactor: chenge logo and favIcon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,9 +19,9 @@ const Navbar = () => {
 		return (
 			<header className='border-b border-white/80 bg-white/80 backdrop-blur-sm sticky top-0 z-50'>
 				<div className='container mx-auto px-4 py-3 sm:py-4 h-16 flex items-center justify-between'>
-					<div className='flex items-center space-x-2'>
-						<Image src={plany_logo} alt='Plany logo' width={35} height={30} />
-						<span className='text-xl sm:text-2xl font-bold text-gray-900'>
+					<div className='flex items-center space-x-2.5'>
+						<Image src={plany_logo} alt='Plany logo' width={40} height={40} className="w-auto h-10" />
+						<span className='text-xl sm:text-2xl font-bold tracking-tight text-gray-900'>
 							Plany
 						</span>
 					</div>
@@ -73,9 +73,9 @@ const Navbar = () => {
 		return (
 			<header className='border-b border-white/80 bg-white/80 backdrop-blur-sm sticky top-0 z-50'>
 				<div className='container mx-auto px-4 py-3 sm:py-4 h-16 flex items-center justify-between'>
-					<div className='flex items-center space-x-2'>
-						<Image src={plany_logo} alt='Plany logo' width={35} height={30} />
-						<span className='text-xl sm:text-2xl font-bold text-gray-900'>
+					<div className='flex items-center space-x-2.5'>
+						<Image src={plany_logo} alt='Plany logo' width={40} height={40} className="w-auto h-10" />
+						<span className='text-xl sm:text-2xl font-bold tracking-tight text-gray-900'>
 							Plany
 						</span>
 					</div>
